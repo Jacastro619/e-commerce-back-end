21094099fe04bc5910b2f60dcc6a3a9b2e79e669
Jacastro619 <jorgecastro619@gmail.com>
2023-11-01 23:02:52 -0700

    Finished adding route endpoint script to route js directory. All endpoints tested on insomnia and work properly

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -56,7 +56,7 @@ router.post("/", (req, res) => {
       // if no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((productTagIds) => res.status(200).json({ productTagIds }))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -107,8 +107,18 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProductById = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "Product Successfully Deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,73 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const getAllTags = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(getAllTags);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const getTagById = await Tag.findByPk(req.params.id, {
+      where: {
+        id: req.params.id,
+      },
+      include: [{ model: Product }],
+    });
+    res.status(200).json(getTagById);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const postId = await Tag.create(req.body);
+    res.status(200).json(postId);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const postId = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "Tag Updated Successfully!" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const postId = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "Tag Successfully Deleted!" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
 });
 
 module.exports = router;
